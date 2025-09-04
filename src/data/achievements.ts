17af27b9d7884787c5a82e1cf58f506ba0f49969
Alnick Clores <aelnickclores@gmail.com>
2025-09-04 22:42:54 +0800

    Added the initial achievements

---
+++ src/data/achievements.ts
@@ -0,0 +1,34 @@
+export const achievements = [
+  {
+    title: "My First Bingoal",
+    description: "Create your first Bingoal card.",
+  },
+  {
+    title: "Dream Catcher",
+    description: "Filled an entire row with goals.",
+  },
+  {
+    title: "Consistency is Key",
+    description: "Completed 5 goals in total.",
+  },
+  {
+    title: "Halfway There",
+    description: "Marked 50% of the card is done.",
+  },
+  {
+    title: "Bingoal Champion",
+    description: "Fully Completed one bingoal card.",
+  },
+  {
+    title: "Getting Organized",
+    description: "Add 10 goals across any cards.",
+  },
+  {
+    title: "Oops, Wrong Goal",
+    description: "Delete a goal you just added.",
+  },
+  {
+    title: "Who needs sleep?",
+    description: "Add or complete a goal after midnight.",
+  },
+];
